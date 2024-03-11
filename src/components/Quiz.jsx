3696a9a4ebb36bd0f8c421560c53769a69c59e7c
suyog200 <suygchari@gmail.com>
2024-03-11 12:16:52 +0530

    added handler to handle quiz completed and show modal

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import QuizCompletedImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnser(answer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +13,24 @@ export default function Quiz() {
     });
   }
 
+  if(quizIsComplete) {
+    return (
+        <div id="summary">
+            <img src={QuizCompletedImg} alt="" />
+            <h2>Quiz completed</h2>
+        </div>
+    )
+  }
+
+  const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffleAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnser(answer)}>
                 {answer}
