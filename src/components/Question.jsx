4b95e1542c3307b9fa6d2ceb07e9e3c92fb4d1ae
suyog200 <suygchari@gmail.com>
2024-03-13 10:18:06 +0530

    fixed bugs on timer issue. and lableing correct and wrong ans.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,43 +4,57 @@ import { useState } from "react";
 import QUESTIONS from "../questions";
 
 export default function Question({
-  questionIndex,  
+  questionIndex,
   onSelectAnswer,
   onSkipAnswer,
 }) {
-    const [answer, setAnswer] = useState({
-        selectedAnswer: '',
-        isCorrect: null,
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectedAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
     });
 
-    function handleSelectedAnswer(answer) {
-        setAnswer({
-            selectedAnswer: answer,
-            isCorrect: null,
-        })
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[questionIndex].answers[0] === answer,
+      });
 
-        setTimeout(() => {
-            setAnswer({
-                selectedAnswer: answer,
-                isCorrect: QUESTIONS[questionIndex].answers[0] === answer,
-            })
+      setTimeout(() => onSelectAnswer(answer), 2000);
+    }, 1000);
+  }
 
-            setTimeout(() => onSelectAnswer(answer), 1000);
-        },1000);
-    }
+  let answerState = "";
 
-   let answerState = '';
-   
-   if(answer.selectedAnswer && answer.isCorrect !== null) {
-         answerState = answer.isCorrect ? 'correct' : 'wrong';
-   } else if(answer.selectedAnswer) {
-         answerState = 'answered';
-   }
-
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[questionIndex].text}</h2>
       <Answers
         answers={QUESTIONS[questionIndex].answers}
